7188a1f74681b0314e89b73d3d2cb21c29526c82
divyapappala <149997549+divyapappala@users.noreply.github.com>
2023-11-13 11:25:01 +0530

    Update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -1,20 +1,20 @@
 import os
-
 file_path = r"C:\Users\lokes\Desktop\assignment\text.txt"
 start_range = 1
 end_range = 100
+
 try:
     with open(file_path, "r") as file:
         content = file.read()
         content_range = content[start_range:end_range]
-        letter_count = sum(c.isalpha() for c in content_range)
+        word_count = len(content_range.split())
+
         print(f"Content in the specified range: {content_range}")
-        print(f"Letter count: {letter_count}")
-        print(content_range)
+        print(f"Word count: {word_count}")
 except FileNotFoundError:
     print(f"File not found: {file_path}")
     with open(file_path, "a") as file:
-            file.write("\n" + content_to_write)
-            print(f"Content written to the file: {content_to_write}")
-except FileNotFoundError:
-        print(f"File not found: {file_path}")
+        file.write("\n" + content_to_write)
+        print(f"Content written to the file: {content_to_write}")
+except Exception as e:
+    print(f"An error occurred: {e}")
