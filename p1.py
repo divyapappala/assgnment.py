360033d96d64d13bb440facff9be15e82b323df1
divyapappala <149997549+divyapappala@users.noreply.github.com>
2024-01-17 16:06:00 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,31 @@
+#type converstion
+# old_age = input("enter new age:")
+# new_age = int(old_age) + 3
+# print(new_age)
+
+# number = 18
+# print(float(18))
+
+# concatination 
+# name =  input("enter your name:")
+# print("hello" + name)
+
+# first = input("enter the number:")
+# second = input("enter the number:")
+# sum = int(first) + int(second)
+# print(sum)
+
+digits = input("enter the numbers seperated by commas:")
+number_list = digits.split(',')
+number_tuple = tuple(number_list)
+print(number_list)
+print(number_tuple)
+
+
+
+
+
+
+
+
+
